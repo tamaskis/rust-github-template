50ce35be2e834a9cb0e58e95e51fc3cb4dc27e6e
tamaskis <tamas.a.kis@outlook.com>
2024-08-17 10:19:33 -0700

    copied over more stuff

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! [![github]](https://github.com/TODO)&ensp;[![crates-io]](https://crates.io/crates/PACKAGE_NAME)&ensp;[![docs-rs]](https://docs.rs/PACKAGE_NAME)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! Template for simple Rust projects hosted on GitHub
+
+// Linter setup.
+#![warn(missing_docs)]
+
+// Linking project modules.
+pub(crate) mod module;
+
+// Re-exports.
+pub use crate::module::example_function;
